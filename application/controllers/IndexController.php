69a8ab1d3656de7a4516802af9a8010606b331b6
piousbox (ativ6 ubuntu15) <piousbox@gmail.com>
2015-09-18 00:02:38 -0700

    the tutorial

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,18 +1,14 @@
 <?php
 
-class IndexController extends Zend_Controller_Action
-{
+class IndexController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         // action body
     }
-
 
 }
 
